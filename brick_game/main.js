e4fd0798277cc74a1b908fcaf90b64de32e4c58d
Adam Kimball <adamchasekimball@gmail.com>
2018-02-26 16:30:49 -0700

    maze problems

--- brick_game/main.js
+++ brick_game/main.js
@@ -170,7 +170,7 @@ function collisionDetection() {
 function drawScore() {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD";
-  ctx.fillText("Score: "+score, 8, 20);
+  ctx.fillText("Score: "+score, 4, 4);
 }
 
 function drawLives() {
